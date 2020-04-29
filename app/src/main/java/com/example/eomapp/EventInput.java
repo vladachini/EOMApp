c6542d228da0a7777c485c7d861cc59baf646bf2
vk <vladimirkovacina@yahoo.com>
2020-04-29 16:57:01 -0400

    posting events progress

---
+++ app/src/main/java/com/example/eomapp/EventInput.java
@@ -0,0 +1,29 @@
+package com.example.eomapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class EventInput extends AppCompatActivity {
+    private EditText title;
+    private EditText category;
+    private EditText date;
+    private EditText time;
+    private EditText details;
+    private EditText author;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_input);
+        title= findViewById(R.id.titleText);
+        String eventT= title.getText().toString();
+        category= findViewById(R.id.categoryText);
+        String eventC= category.getText().toString();
+        date= findViewById(R.id.dateText);
+        
+
+
+
+    }
+}
--- app/src/main/java/com/example/eomapp/HomeFeed.java
+++ app/src/main/java/com/example/eomapp/HomeFeed.java
@@ -2,16 +2,26 @@ package com.example.eomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeFeed extends AppCompatActivity {
-
+    private Button post;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_feed);
         //type code here
-
+        post=(Button) findViewById(R.id.createBtn);
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), EventInput.class);
+                startActivity(startIntent);
+            }
+        });
 
     }
 }
