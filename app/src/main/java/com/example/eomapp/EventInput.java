b5068b44bfd691e068e6cae60de0c7d58d48b89a
vk <vladimirkovacina@yahoo.com>
2020-04-30 13:13:03 -0400

    before signup 2 merge

--- app/src/main/java/com/example/eomapp/EventInput.java
+++ app/src/main/java/com/example/eomapp/EventInput.java
@@ -3,7 +3,10 @@ package com.example.eomapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EventInput extends AppCompatActivity {
     private EditText title;
@@ -12,15 +15,38 @@ public class EventInput extends AppCompatActivity {
     private EditText time;
     private EditText details;
     private EditText author;
+    private Button post;
+    DatabaseHelper db= new DatabaseHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_input);
-        title= findViewById(R.id.titleText);
-        String eventT= title.getText().toString();
-        category= findViewById(R.id.categoryText);
-        String eventC= category.getText().toString();
-        date= findViewById(R.id.dateText);
+        post= findViewById(R.id.postBtn);
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title= findViewById(R.id.titleText);
+                String eventTitle= title.getText().toString();
+                category= findViewById(R.id.categoryText);
+                String eventCategory= category.getText().toString();
+                date= findViewById(R.id.dateText);
+                String eventDate= date.getText().toString();
+                time=findViewById(R.id.timeText);
+                String eventTime= time.getText().toString();
+                details= findViewById(R.id.detailsText);
+                String eventDetails= details.getText().toString();
+                author= findViewById(R.id.authorText);
+                String eventAuthor= author.getText().toString();
+                Events e= new Events (eventTitle,eventCategory, eventDate, eventTime, eventDetails);
+                if(eventTitle.equals("")|| eventCategory.equals("")|| eventDate.equals("") || eventTime.equals("") ){
+                    Toast.makeText(getApplicationContext(), "Make Sure Fields aren't Empty", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    db.insertEvent(eventTitle,eventCategory,eventDate,eventTime,eventDetails,eventAuthor);
+                }
+            }
+        });
+
 
 
 
--- app/src/main/java/com/example/eomapp/Events.java
+++ app/src/main/java/com/example/eomapp/Events.java
@@ -3,20 +3,64 @@ package com.example.eomapp;
 public class Events {
     String title;
     String details;
-    int date;
-    static int idCounter=0;
-    String iD;
-    int time; //might need to change from int into string later
+    String date;
+    String time; //might need to change from int into string later
     String category;
     boolean saved = false;
 
-    public Events(String t, String d, int dt, int tm, String c){
+    public Events(String t, String c, String dt, String tm, String d){
         title=t;
         details=d;
         date=dt;
         time=tm;
         category=c;
-        idCounter++;
-        iD="e" + idCounter;
+
+    }
+    public Events(String t, String c, String dt, String tm){
+        title=t;
+        date=dt;
+        time=tm;
+        category=c;
+
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
